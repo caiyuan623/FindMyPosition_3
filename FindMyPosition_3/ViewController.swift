704b185825705c83afd94d1b2bd3f77a7acb993c
caiyuan623 <15051987332@163.com>
2017-08-07 15:04:49 +0800

    添加注释

--- FindMyPosition_3/ViewController.swift
+++ FindMyPosition_3/ViewController.swift
@@ -7,19 +7,87 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    let geocoder = CLGeocoder()
+    let locationLabel = UILabel()
+    let locationStrLabel = UILabel()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        let bgImageView = UIImageView(frame: self.view.bounds)
+        bgImageView.image = #imageLiteral(resourceName: "phoneBg")
+        self.view.addSubview(bgImageView)
+        
+        let blurEffect: UIBlurEffect = UIBlurEffect(style: .light)
+        let blurView: UIVisualEffectView = UIVisualEffectView(effect: blurEffect)
+        blurView.frame = self.view.bounds
+        self.view.addSubview(blurView)
+        
+        locationManager.delegate = self
+        
+        locationLabel.frame = CGRect(x: 0, y: 50, width: self.view.bounds.width, height: 100)
+        locationLabel.textAlignment = .center
+        locationLabel.textColor = UIColor.white
+        self.view.addSubview(locationLabel)
+        
+        locationStrLabel.frame = CGRect(x: 0, y: 100, width: self.view.bounds.width, height:50)
+        locationStrLabel.textAlignment = .center
+        locationStrLabel.textColor = UIColor.white
+        self.view.addSubview(locationStrLabel)
+        
+        let findMyLocationBtn = UIButton(type: .custom)
+        findMyLocationBtn.frame = CGRect(x: 50, y: self.view.bounds.height - 80, width: self.view.bounds.width - 100, height: 50)
+        findMyLocationBtn.setTitle("Find My Position", for: UIControlState.normal)
+        findMyLocationBtn.setTitleColor(UIColor.white, for: UIControlState.normal)
+        findMyLocationBtn.addTarget(self, action: #selector(findMyLocation), for: UIControlEvents.touchUpInside)
+        self.view.addSubview(findMyLocationBtn)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func findMyLocation() {
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    //获取经纬度
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locations : NSArray = locations as NSArray
+        let currentLocations = locations.lastObject as! CLLocation
+        let locationStr = "lat:\(currentLocations.coordinate.latitude) lng:\(currentLocations.coordinate.longitude)"
+        locationLabel.text = locationStr
+        reverseGeocode(location:currentLocations)
+        locationManager.stopUpdatingHeading()
 
+    }
+    
+    //将经纬度换为城市名
+    func reverseGeocode(location:CLLocation) {
+        geocoder.reverseGeocodeLocation(location) { (placeMark, error) in
+            if error == nil {
+                let temArray = placeMark! as NSArray
+                let mark = temArray.firstObject as! CLPlacemark
+                
+//                now  begins the reverse geocode
+                let addressDictionary = mark.addressDictionary! as NSDictionary
+                let country = addressDictionary.value(forKey: "Country") as! String
+                let city = addressDictionary.object(forKey: "City") as! String
+                let street = addressDictionary.object(forKey: "Street") as! String
+                
+                let finalAddress = "\(street),\(city),\(country)"
+                self.locationStrLabel.text = finalAddress
+            }
+        }
+    }
 
 }
 
